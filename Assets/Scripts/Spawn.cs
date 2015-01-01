828732bb7344f09d3868465d5ae81e2b76a26eb8
JoelLimes <13505@ma-web.nl>
2015-01-01 01:09:51 +0100

    23/november 2015

---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour 
+{
+	[SerializeField]
+	private GameObject Enemy;
+
+	[SerializeField]
+	private float spawnTime;
+
+	// Use this for initialization
+	void Start () 
+	{
+		InvokeRepeating ("SpawnEnemy", spawnTime, spawnTime);
+//		Instantiate (Enemy); 
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	void SpawnEnemy()
+	{
+		var newEnemy = GameObject.Instantiate(Enemy);
+	}
+}
---
+++ Assets/Scripts/TurretAI.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretAI : MonoBehaviour 
+{
+	public float radius = 3;
+	public Transform Spawnpos;
+	public Transform bullet;
+	
+	private float waitTime = 0.5f;
+
+	void Start()
+	{
+		
+	}
+
+	void Update()
+	{
+
+		transform.rotation = Quaternion.AngleAxis (GetAngle (GetTarget()) + 90, Vector3.forward);
+		waitTime += Time.deltaTime;
+		if (waitTime > .6f) 
+		{
+			Instantiate(bullet,Spawnpos.position, Spawnpos.rotation);
+			waitTime = 0;
+//			AudioSource audio = GetComponent<AudioSource>();
+//			audio.Play();
+		}
+	}
+
+
+	// draw radius and give color
+	void OnDrawGizmos() {
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(transform.position, radius);
+	}
+
+	Vector3 GetTarget() {
+		Collider2D[] colliders = Physics2D.OverlapCircleAll (transform.position, 3, LayerMask.GetMask("Enemy"));
+		GameObject closestTarget = null;
+		float smallestDistance = int.MaxValue;
+
+		foreach (Collider2D coll in colliders) {
+			float distance = Vector3.Distance(transform.position, coll.transform.position);
+
+			if(distance < smallestDistance) {
+				closestTarget = coll.gameObject;
+				smallestDistance = distance;
+			}
+		}
+
+		return closestTarget.transform.position;
+	}
+
+
+	float GetAngle(Vector3 target) {
+		Vector3 distance = transform.position - target;
+		float angle = Mathf.Atan2(distance.y, distance.x) * Mathf.Rad2Deg;
+		return angle;
+	}
+
+}
+ 
