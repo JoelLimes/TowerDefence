6ba5149f0801ac0b73203c8167c8b5adf127bfdb
Icedia <daniel1995@live.nl>
2015-11-09 14:17:07 +0100

    Git Update
    Beginning of turret creation

---
+++ TowerDefence/Assets/EnemyHealth.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHealth : MonoBehaviour
+{
+    private float _maxHealth = 100;
+    private float _currentHealth;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ TowerDefence/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private float _movementSpeed;
+
+    private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 newVelocity = new Vector2(_movementSpeed, 0f);
+        rb2d.velocity = newVelocity;
+	
+	}
+}
