89c6f76902c1b0d6993a7faefaab52fb2d9fb62e
JoelLimes <13505@ma-web.nl>
2015-12-01 08:41:41 +0100

    01/12/2015
    fixed: Everything (behalve Wallet system)

---
+++ Assets/Scripts/HomeHealth.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+/**
+* ...
+* @author Joel Limes
+* ...
+**/
+
+public class HomeHealth : MonoBehaviour 
+{
+	
+	public float maxHealth = 100f;
+	public float currentHealth = 0f;
+	public GameObject healthBar;
+	
+	void Start () 
+	{
+		currentHealth = maxHealth;
+	}
+	
+	void Update () 
+	{
+		
+	}
+	
+	void decreaseHealth()
+	{
+		currentHealth -= 10f;
+		float calc_Health = currentHealth / maxHealth;
+		SetHealthBar (calc_Health);
+	}
+	
+	public void SetHealthBar(float myHealth)
+	{
+		healthBar.transform.localScale = new Vector3 (myHealth, healthBar.transform.localScale.y, healthBar.transform.localScale.z); 
+	}
+	
+	void OnTriggerEnter2D(Collider2D coll)
+	{
+		if (coll.gameObject.tag == "enemy") 
+		{
+			decreaseHealth();
+		}
+		
+		if(currentHealth <= 0)
+		{
+			Destroy(this.gameObject);
+			Application.LoadLevel("GameOverScene");
+		}
+	}
+}
--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -15,6 +15,7 @@ public class PauseMenu : MonoBehaviour
 	[SerializeField]
 	private GameObject pauseMenuCanvas;
 
+
 	// Update is called once per frame
 	void Update () 
 	{
